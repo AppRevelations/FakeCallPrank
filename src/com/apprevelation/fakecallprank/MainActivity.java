db2a8e8c43e539e0f649a942a5ac805a5c996c69
Lakshay Chhikara <lakshay.chhikara09@gmail.com>
2014-07-21 18:37:28 +0530

    Removed android-support-v4.jar from libs folder as it is already included in appcompat library which has been referenced in the project.

--- src/com/apprevelation/fakecallprank/MainActivity.java
+++ src/com/apprevelation/fakecallprank/MainActivity.java
@@ -1,20 +1,15 @@
 package com.apprevelation.fakecallprank;
 
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-	startService(new Intent(MainActivity.this, PhoneState.class));	
-    
-    }
+		startService(new Intent(MainActivity.this, PhoneState.class));	
+	}
 }
--- src/com/apprevelation/fakecallprank/PhoneState.java
+++ src/com/apprevelation/fakecallprank/PhoneState.java
@@ -10,111 +10,105 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class PhoneState extends Service {
-    private CallStateListener mCallStateListener = new CallStateListener();
-    private TelephonyManager mTelephonyManager;
-    private int mCallState;
+	private CallStateListener mCallStateListener = new CallStateListener();
+	private TelephonyManager mTelephonyManager;
+	private int mCallState;
 
-    @Override
-    public void onCreate() {
-    super.onCreate();
-    mTelephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-    mCallState = mTelephonyManager.getCallState();
-    mTelephonyManager.listen(mCallStateListener, PhoneStateListener.LISTEN_CALL_STATE);
-    }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mTelephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+		mCallState = mTelephonyManager.getCallState();
+		mTelephonyManager.listen(mCallStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+	}
 
-    @Override
-    public void onDestroy() {
-        Log.d("onDestroy", "onDestroy");
-    mTelephonyManager.listen(mCallStateListener, PhoneStateListener.LISTEN_NONE);
-    super.onDestroy();
-    }
+	@Override
+	public void onDestroy() {
+		Log.d("onDestroy", "onDestroy");
+		mTelephonyManager.listen(mCallStateListener, PhoneStateListener.LISTEN_NONE);
+		super.onDestroy();
+	}
 
-    @Override
-    public IBinder onBind(Intent intent) {
-    return null; //-- not a bound service--
-    }
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null; //-- not a bound service--
+	}
 
-    private final class CallStateListener extends PhoneStateListener {
-    @Override
-    public void onCallStateChanged(int state, String incomingNumber) {
+	private final class CallStateListener extends PhoneStateListener {
+		@Override
+		public void onCallStateChanged(int state, String incomingNumber) {
 
-        switch (mCallState) {
+			switch (mCallState) {
 
-            case TelephonyManager.CALL_STATE_IDLE:
-                if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
-                	Toast.makeText(getApplicationContext(),"idle --> off hook = new outgoing call", Toast.LENGTH_SHORT).show();
-                    Log.d("state", "idle --> off hook = new outgoing call");
-                    // idle --> off hook = new outgoing call
-                    //triggerSenses(Sense.CallEvent.OUTGOING);
-                } else if (state == TelephonyManager.CALL_STATE_RINGING) {
-                	
-                	
-                	//change the incoming number here 
-                	//change the log as well
-                	
-                	
-                	Toast.makeText(getApplicationContext(),"idle --> ringing = new incoming call", Toast.LENGTH_SHORT).show();
-                	Log.d("state", "idle --> ringing = new incoming call");
-                    // idle --> ringing = new incoming call
-                    //triggerSenses(Sense.CallEvent.INCOMING);
-                }
-                break;
+				case TelephonyManager.CALL_STATE_IDLE:
+					if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
+						Toast.makeText(getApplicationContext(),"idle --> off hook = new outgoing call", Toast.LENGTH_SHORT).show();
+						Log.d("state", "idle --> off hook = new outgoing call");
+						// idle --> off hook = new outgoing call
+						//triggerSenses(Sense.CallEvent.OUTGOING);
+					} else if (state == TelephonyManager.CALL_STATE_RINGING) {
 
-            case TelephonyManager.CALL_STATE_OFFHOOK:
-                if (state == TelephonyManager.CALL_STATE_IDLE) {
-                	
-                	//change here as well
-                	
-                	Toast.makeText(getApplicationContext(),"off hook --> idle  = disconnected", Toast.LENGTH_SHORT).show();
-                    Log.d("state", "off hook --> idle  = disconnected");
-                    // off hook --> idle  = disconnected
-                    //triggerSenses(Sense.CallEvent.ENDED);
-                } else if (state == TelephonyManager.CALL_STATE_RINGING) {
-                	
-                	
-                	//change here too
-                	
-                	Toast.makeText(getApplicationContext(), "off hook --> ringing = another call waiting", Toast.LENGTH_SHORT).show();
-                    Log.d("state", "off hook --> ringing = another call waiting");
-                    // off hook --> ringing = another call waiting
-                    //triggerSenses(Sense.CallEvent.WAITING);
-                }
-                Toast.makeText(getApplicationContext(),"CALL_STATE_OFFHOOK", Toast.LENGTH_SHORT).show();
-                Log.d("CALL_STATE_OFFHOOK", String.valueOf(state));
-                break;
+						//change the incoming number here 
+						//change the log as well
 
-            case TelephonyManager.CALL_STATE_RINGING:
-                if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
-                	
-                	
-                	//chane here to
-                	
-                	Toast.makeText(getApplicationContext(),"ringing --> off hook = received", Toast.LENGTH_SHORT).show();
-                    Log.d("state", "ringing --> off hook = received");
-                    // ringing --> off hook = received
-                    //triggerSenses(Sense.CallEvent.RECEIVED);
-                } else if (state == TelephonyManager.CALL_STATE_IDLE) {
-                	
-                	//change log
-                	//change notification bar
-                	//change call screen
-                	
-                	
-                	Toast.makeText(getApplicationContext(),"ringing --> idle = missed call", Toast.LENGTH_SHORT).show();
-                    Log.d("state", "ringing --> idle = missed call");
-                    // ringing --> idle = missed call
-                    //triggerSenses(Sense.CallEvent.MISSED);
-                }
-                break;
-        }
+						Toast.makeText(getApplicationContext(),"idle --> ringing = new incoming call", Toast.LENGTH_SHORT).show();
+						Log.d("state", "idle --> ringing = new incoming call");
+						// idle --> ringing = new incoming call
+						//triggerSenses(Sense.CallEvent.INCOMING);
+					}
+					break;
 
-        mCallState = state;
-    }
-    }
+				case TelephonyManager.CALL_STATE_OFFHOOK:
+					if (state == TelephonyManager.CALL_STATE_IDLE) {
 
-    public static void init(Context c) {
-        c.startService(new Intent(c, PhoneState.class));
-    Log.d("Service enabled","Service enabled: " + true);
-    }
+						//change here as well
+
+						Toast.makeText(getApplicationContext(),"off hook --> idle  = disconnected", Toast.LENGTH_SHORT).show();
+						Log.d("state", "off hook --> idle  = disconnected");
+						// off hook --> idle  = disconnected
+						//triggerSenses(Sense.CallEvent.ENDED);
+					} else if (state == TelephonyManager.CALL_STATE_RINGING) {
+
+						//change here too
+
+						Toast.makeText(getApplicationContext(), "off hook --> ringing = another call waiting", Toast.LENGTH_SHORT).show();
+						Log.d("state", "off hook --> ringing = another call waiting");
+						// off hook --> ringing = another call waiting
+						//triggerSenses(Sense.CallEvent.WAITING);
+					}
+					Toast.makeText(getApplicationContext(),"CALL_STATE_OFFHOOK", Toast.LENGTH_SHORT).show();
+					Log.d("CALL_STATE_OFFHOOK", String.valueOf(state));
+					break;
+
+				case TelephonyManager.CALL_STATE_RINGING:
+					if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
+
+						//change here to
+
+						Toast.makeText(getApplicationContext(),"ringing --> off hook = received", Toast.LENGTH_SHORT).show();
+						Log.d("state", "ringing --> off hook = received");
+						// ringing --> off hook = received
+						//triggerSenses(Sense.CallEvent.RECEIVED);
+					} else if (state == TelephonyManager.CALL_STATE_IDLE) {
+
+						//change log
+						//change notification bar
+						//change call screen
+
+						Toast.makeText(getApplicationContext(),"ringing --> idle = missed call", Toast.LENGTH_SHORT).show();
+						Log.d("state", "ringing --> idle = missed call");
+						// ringing --> idle = missed call
+						//triggerSenses(Sense.CallEvent.MISSED);
+					}
+					break;
+			}
+
+			mCallState = state;
+		}
+	}
+
+	public static void init(Context c) {
+		c.startService(new Intent(c, PhoneState.class));
+		Log.d("Service enabled","Service enabled: " + true);
+	}
 }
-
